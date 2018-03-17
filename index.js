432ea678b20fa4f22619777e4b72237c2f7c5f27
eddy <eddy@stlwolf.com>
2018-03-17 11:30:23 +0900

    fix logger

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const express = require('express');
+const app = express();
+
 const log4js = require('log4js');
 const logger = log4js.getLogger();
-
-let app = require('express')();
+logger.level = 'debug'; // default level is OFF - which means no logs at all.
 
 app.get('/', function (req, res) {
   res.send('GET API hello');
