30a5bb2738279c3b2da881a5ac5d6add0673e0a0
eddy <eddy@stlwolf.com>
2018-03-18 14:20:18 +0900

    index sample 2type

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 const log4js = require('log4js');
 const logger = log4js.getLogger();
 logger.level = 'debug'; // default level is OFF - which means no logs at all.
 
-app.get('/', function (req, res) {
-  res.send('GET API hello');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+const renderApp = () => `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+  <h3>Hello from express</h3>
+  <div id="app"></div>
+</body>
+</html>
+`;
+
+app.get('/render', function (req, res) {
+  res.send(renderApp());
   logger.debug("GET hello !!!");
 });
 
