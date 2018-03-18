632b144883ed9a19e8a631eeb163382f3bfa1bbc
eddy <eddy@stlwolf.com>
2018-03-18 15:18:42 +0900

    try sample code

---
+++ public/app.js
@@ -0,0 +1,13 @@
+let app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue sample !!'
+  }
+});
+
+let app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loaded this page on ' + new Date().toLocaleString()
+  }
+});
